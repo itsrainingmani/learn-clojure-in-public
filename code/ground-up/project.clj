87a78a16a67755317fbfe0b5008902a354cf2a41
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-06-27 03:17:18 -0400

    Change project information

--- code/ground-up/project.clj
+++ code/ground-up/project.clj
@@ -1,8 +1,8 @@
 (defproject ground-up "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :description "Project for Clojure from the Ground Up"
+  :url "https://github.com/itsrainingmani/learn-clojure-in-public"
+  :license {:name "MIT"
+            :url  "https://mit-license.org"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
   :main ^:skip-aot ground-up.core
   :target-path "target/%s"
