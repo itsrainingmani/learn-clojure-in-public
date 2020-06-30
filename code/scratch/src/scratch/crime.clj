90ff5352bebec786becbc4f19a4f608df075ad36
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-06-29 20:42:41 -0500

    Complete code for chapter 7

--- code/scratch/src/scratch/crime.clj
+++ code/scratch/src/scratch/crime.clj
@@ -4,7 +4,7 @@
 (defn load-json
   "Given a filename, reads a JSON file and returns it, parsed, with keywords"
   [file]
-  (json/parse-string (slurp file) true))
+  (json/parse-string (slurp file) true)) ;; the true arg to parse-string coerces keys into keywords
 
 (def fips
   "A Map of FIPS Codes to their country names"
@@ -21,6 +21,13 @@
        load-json
        (sort-by :driving_under_influence)
        (take-last 10)
-       (map #(select-keys % [:driving_under_influence
-                             :fips_county_code
-                             :fips_state_code]))))
+       (map (fn [county]
+              [(fips (fips-code county))
+               (:driving_under_influence county)]))
+       (into {})
+       (sort-by key)))
+
+(defn fips-code
+  "Converts county data into fips code"
+  [county]
+  (str (:fips_state_code county) (:fips_county_code county)))
