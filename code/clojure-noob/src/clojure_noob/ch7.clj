eb34f038982d4e9bce634dd693ab7e51dc804aab
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-07-11 00:58:26 -0500

    Add some more info about clojure evaluation

--- code/clojure-noob/src/clojure_noob/ch7.clj
+++ code/clojure-noob/src/clojure_noob/ch7.clj
@@ -15,3 +15,17 @@
 ;; Clojure data structures -> evaluated
 
 ;; Homoiconic -> reason about the code as a set of data structures that you can manipulate programmatically
+
+;; Lisps evaluate native data structures
+;; Clojure still evaluates tree structures, but the trees are structured using Clojure liss and the nodes are Clojure values
+
+;; Clojure reader converts code (+ 1 (* 6 7)) into a nested list.
+;; Clojure evaluator takes that data as input and produces a result. (also compiles to the JVM bytecode)
+
+(def addition-list (list + 1 2))
+(eval addition-list)
+;; => 3
+
+;; Send program's data structures directly to the clojure evaluator with 'eval'
+
+;; Reader 
