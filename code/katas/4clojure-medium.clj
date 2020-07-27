e025ac22819d07ccf272d2b61620c81d157fab8f
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-07-27 04:13:54 -0500

    Complete problem No. 102 intoCamelCase

--- code/katas/4clojure-medium.clj
+++ code/katas/4clojure-medium.clj
@@ -104,3 +104,11 @@
        #(foo x (+ 2 y))))]
   (trampoline foo [] 1))
 ;; => [1 3 5 7 9 11]
+
+;; No. 102 intoCamelCase
+;; When working with java, you often need to create an object with fieldsLikeThis, but you'd rather work with a hashmap that has :keys-like-this until it's time to convert. Write a function which takes lower-case hyphen-separated strings and converts them to camel-case strings.
+
+(defn intoCamelCase
+  [s]
+  (let [spl (clojure.string/split s #"-")]
+    (clojure.string/join (cons (first spl) (map clojure.string/capitalize (rest spl))))))
