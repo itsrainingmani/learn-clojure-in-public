704b5aaa463854eaeb69b9721bde53a3b44e0ba0
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2020-07-27 04:07:43 -0500

    Complete No. 76 - Intro to Trampoline

--- code/katas/4clojure-medium.clj
+++ code/katas/4clojure-medium.clj
@@ -92,3 +92,15 @@
 (defn flip-out
   [i]
   (fn [x y] (i y x)))
+
+;; No. 76 Intro to Trampoline
+;; The trampoline function takes a function f and a variable number of parameters. Trampoline calls f with any parameters that were supplied. If f returns a function, trampoline calls that function with no arguments. This is repeated, until the return value is not a function, and then trampoline returns that non-function value. This is useful for implementing mutually recursive algorithms in a way that won't consume the stack.
+(letfn
+  [(foo [x y] #(bar (conj x y) y))
+   (bar
+     [x y]
+     (if (> (last x) 10)
+       x
+       #(foo x (+ 2 y))))]
+  (trampoline foo [] 1))
+;; => [1 3 5 7 9 11]
